fd03edb49264e8d80dc22aa29f2e29f16a2ebf5e
KuleSang <anagaiyyy@gmail.com>
2026-01-22 18:18:38 +0800

    Add files via upload

---
+++ assets/LanguageSwitcher-kioW6mFI.js
@@ -0,0 +1,21 @@
+import{c as r}from"./createLucideIcon-DFrmIa5Z.js";import{u as h,c as k,a as g,o as _,w as t,k as a,i as x,j as f,t as v,l as o,p as s}from"./index-DmLllGbO.js";import{_ as w}from"./_plugin-vue_export-helper-BKqSRVk_.js";/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=r("languages",[["path",{d:"m5 8 6 6",key:"1wu5hv"}],["path",{d:"m4 14 6-6 2-3",key:"1k1g8d"}],["path",{d:"M2 5h12",key:"or177f"}],["path",{d:"M7 2h1",key:"1t2jsx"}],["path",{d:"m22 22-5-10-5 10",key:"don7ne"}],["path",{d:"M14 18h6",key:"1m8k6r"}]]);/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const V=r("moon",[["path",{d:"M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401",key:"kfwtm"}]]);/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=r("server",[["rect",{width:"20",height:"8",x:"2",y:"2",rx:"2",ry:"2",key:"ngkwjq"}],["rect",{width:"20",height:"8",x:"2",y:"14",rx:"2",ry:"2",key:"iecqi9"}],["line",{x1:"6",x2:"6.01",y1:"6",y2:"6",key:"16zg32"}],["line",{x1:"6",x2:"6.01",y1:"18",y2:"18",key:"nzw8ys"}]]);/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const I=r("sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]),S={class:"lang-text"},N={__name:"LanguageSwitcher",setup(b){const{locale:c}=h(),l={"zh-CN":"简体中文","en-US":"English","vi-VN":"Tiếng Việt","ja-JP":"日本語"},d=k(()=>l[c.value]||c.value),i=({key:p})=>{c.value=p,localStorage.setItem("locale",p)};return(p,e)=>{const m=o("a-button"),n=o("a-menu-item"),y=o("a-menu"),u=o("a-dropdown");return _(),g(u,{trigger:["click"],placement:"bottomRight"},{overlay:t(()=>[a(y,{onClick:i},{default:t(()=>[a(n,{key:"zh-CN"},{default:t(()=>[...e[0]||(e[0]=[s("简体中文",-1)])]),_:1}),a(n,{key:"en-US"},{default:t(()=>[...e[1]||(e[1]=[s("English",-1)])]),_:1}),a(n,{key:"vi-VN"},{default:t(()=>[...e[2]||(e[2]=[s("Tiếng Việt",-1)])]),_:1}),a(n,{key:"ja-JP"},{default:t(()=>[...e[3]||(e[3]=[s("日本語",-1)])]),_:1})]),_:1})]),default:t(()=>[a(m,{type:"text",class:"lang-btn"},{default:t(()=>[a(f(M),{size:20}),x("span",S,v(d.value),1)]),_:1})]),_:1})}}},q=w(N,[["__scopeId","data-v-bf0b3c15"]]);export{q as L,V as M,C as S,I as a};
---
+++ assets/x-BD7iIADi.js
@@ -0,0 +1,11 @@
+import{c as o}from"./createLucideIcon-DFrmIa5Z.js";/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=o("chevron-down",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=o("x",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{n as C,t as X};
